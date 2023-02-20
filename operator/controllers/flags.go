a9d3ad8b84c2589eb52b45f3d9c64185cd64decb
6za <53096417+6za@users.noreply.github.com>
2023-02-20 15:38:39 +0000

    fix: correct usage of ldflags
    Signed-off-by: 6za <53096417+6za@users.noreply.github.com>

--- operator/controllers/flags.go
+++ operator/controllers/flags.go
@@ -1,7 +1,8 @@
 package controllers
 
 // !TODO: Set by LD Flags on Dockerfile
-// var NamespaceJobs = "default"
-var NamespaceJobs = "operator-system"
+var NamespaceJobs = "default"
+
+// var NamespaceJobs = "operator-system"
 var ServiceAccountJob = "docker-agent-sa"
 var ImageJob = "6zar/docker-agent:latest"
